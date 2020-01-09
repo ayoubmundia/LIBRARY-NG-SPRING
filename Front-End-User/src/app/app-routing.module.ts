304e8124616c3ea0c5672384e1e2890044630243
shadowlight98 <anasachik4@gmail.com>
2020-01-09 13:05:08 +0100

    book-details v0.0

--- Front-End-User/src/app/app-routing.module.ts
+++ Front-End-User/src/app/app-routing.module.ts
@@ -6,10 +6,12 @@ import { HomeComponent } from './home/home.component';
 import { SearchBarComponent } from './search-bar/search-bar.component'
 import { BookListComponent } from './book-list/book-list.component';
 import { ProfilComponent } from './profil/profil.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
 
 const routes: Routes = [
     {path: 'singin', component: SingInComponent},
     {path: 'booklist', component: BookListComponent},
+    {path: 'bookdetails/:id', component: BookDetailsComponent},
     {path: 'singup', component: SingUpComponent},
     {path: 'profil', component: ProfilComponent},
     {path: '', component: HomeComponent}
@@ -20,4 +22,4 @@ const routes: Routes = [
     exports: [RouterModule]
 })
 export class AppRoutingModule {}
-export const routingComponents = [SingInComponent,SingUpComponent,HomeComponent,SearchBarComponent,BookListComponent,ProfilComponent]
+export const routingComponents = [SingInComponent,SingUpComponent,HomeComponent,BookDetailsComponent,SearchBarComponent,BookListComponent,ProfilComponent]
